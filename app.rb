75b6b32685ad9ffc777c9873f13edfb3275efc76
ig <ig866@mail.ru>
2019-08-06 16:04:11 +0300

    Add mail /contacks

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 
+
 get '/' do
 	erb "Hello!"
 end
@@ -18,21 +19,7 @@ end
 get '/contacts' do
 	erb :contacts
 end
-
-post '/contacts' do
-  @customer_mail = params[:customer_mail]
-
-h1 ={:customer_mail =>'Введите почту'}
-
-@error = h1.select {|key,_|params[key] == ""}.values.join(",")
-
-  if @error != ''
-  return erb :contacts
-end
-
-  erb "ok!,#{@customer_mail}"
-end
-
+#########################################################
 post '/visit' do
 
   @username = params[:username]
@@ -62,4 +49,51 @@ post '/visit' do
   end
 
   erb "ok!,#{@username},#{@phone},#{@datetime},#{@barber},#{@color}"
+end
+
+post '/contacts' do
+  require "pony"
+
+  #body = "1111111111111#{@username},#{@phone},#{@datetime},#{@barber},#{@color}"
+
+ @barbershop_mail_pass = params[:barbershop_mail_pass]
+ @customer_mail = params[:customer_mail]
+
+  ##########################################################
+  Pony.mail({
+
+                :subject => "Вы записаны в наш Barbershop",
+                :body => 'йобанаврот оно работает',
+                :to => @customer_mail,
+                :from => 'ihor.boiko1@gmail.com',
+
+                via: :smtp,
+                via_options:
+                    {
+                        domain: 'gmail.com',
+                        address: 'smtp.gmail.com',
+                        port:  '587',
+                        :user_name => 'ihor.boiko1',
+                        :password => @barbershop_mail_pass,
+                        :authentication => :plain
+                    }
+            })
+
+  ################################
+
+  h1 ={:customer_mail =>'Введите почту'}
+
+  @error = h1.select {|key,_|params[key] == ""}.values.join(",")
+
+  if @customer_mail == ''
+    @error = 'Ошибка: Введите почту!'
+    return erb :contacts
+  end
+
+  if @error != ''
+    return erb :contacts
+  end
+
+  erb "ok!,#{@username},#{@phone},#{@datetime},#{@barber},#{@color}"
+  erb "ok! мы отправили подтвержение ,#{@customer_mail}"
 end
